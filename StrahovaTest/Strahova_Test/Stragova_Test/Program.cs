2ae357cf0018f554ab56b6dcd38059f180ff4c6a
alest1448 <118421044+alest1448@users.noreply.github.com>
2022-11-28 16:00:20 +0900

    Initial commit

---
+++ StrahovaTest/Strahova_Test/Stragova_Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+namespace Stragova_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp.GetInspector();//вывод имени инспектора
+            Console.WriteLine();
+            FunctionInsp.GetСarInspection(); //вывод названия инспекции
+            Console.WriteLine();
+            FunctionInsp.SetInspector(); //изменить имя главного инспектора при совпадении с именами сотрудников
+            Console.WriteLine();
+            FunctionInsp.GetWorker(); //вывод всех сотрудников
+            Console.WriteLine();
+            FunctionInsp.AddWorker();//добавление нового сотрудника
+            Console.WriteLine();
+            FunctionInsp.GenerateNumber();//генерация номера
+        
+          
+        }
+    }
+}
